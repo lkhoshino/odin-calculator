a2a0aaedb0d5260b8ff2a0973f2b1c7920529c6f
lkhoshino <lucaskenzoh@gmail.com>
2022-01-16 18:07:04 -0300

    functional button + display

---
+++ scripts.js
@@ -0,0 +1,98 @@
+function operate(){
+
+}
+
+const button1 = document.querySelector(".button1");
+button1.addEventListener("click", button1Pressed);
+const button2 = document.querySelector(".button2");
+button2.addEventListener("click", button2Pressed);
+const button3 = document.querySelector(".button3");
+button3.addEventListener("click", button3Pressed);
+const button4 = document.querySelector(".button4");
+button4.addEventListener("click", button4Pressed);
+const button5 = document.querySelector(".button5");
+button5.addEventListener("click", button5Pressed);
+const button6 = document.querySelector(".button6");
+button6.addEventListener("click", button6Pressed);
+const button7 = document.querySelector(".button7");
+button7.addEventListener("click", button7Pressed);
+const button8 = document.querySelector(".button8");
+button8.addEventListener("click", button8Pressed);
+const button9 = document.querySelector(".button9");
+button9.addEventListener("click", button9Pressed);
+const button0 = document.querySelector(".button0");
+button0.addEventListener("click", button0Pressed);
+
+const buttonAdd = document.querySelector(".add");
+buttonAdd.addEventListener("click", buttonAddPressed);
+const buttonSub = document.querySelector(".sub");
+buttonSub.addEventListener("click", buttonSubPressed);
+const buttonMulti = document.querySelector(".multi");
+buttonMulti.addEventListener("click", buttonMultiPressed);
+const buttonDivis = document.querySelector(".divis");
+buttonDivis.addEventListener("click", buttonDivisPressed);
+
+
+const display = document.querySelector(".display");
+
+let currentValue ="";
+
+function button1Pressed(){
+    display.innerHTML = currentValue.concat("1");
+    currentValue = display.innerHTML;
+}
+function button2Pressed(){
+    display.innerHTML = currentValue.concat("2");
+    currentValue = display.innerHTML;
+}
+function button3Pressed(){
+    display.innerHTML = currentValue.concat("3");
+    currentValue = display.innerHTML;
+}
+function button4Pressed(){
+    display.innerHTML = currentValue.concat("4");
+    currentValue = display.innerHTML;
+}
+function button5Pressed(){
+    display.innerHTML = currentValue.concat("5");
+    currentValue = display.innerHTML;
+}
+function button6Pressed(){
+    display.innerHTML = currentValue.concat("6");
+    currentValue = display.innerHTML;
+}
+function button7Pressed(){
+    display.innerHTML = currentValue.concat("7");
+    currentValue = display.innerHTML;
+}
+function button8Pressed(){
+    display.innerHTML = currentValue.concat("8");
+    currentValue = display.innerHTML;
+}
+function button9Pressed(){
+    display.innerHTML = currentValue.concat("9");
+    currentValue = display.innerHTML;
+}
+function button0Pressed(){
+    display.innerHTML = currentValue.concat("0");
+    currentValue = display.innerHTML;
+}
+
+
+function buttonAddPressed(){
+    display.innerHTML = currentValue.concat("+");
+    currentValue = display.innerHTML;
+}
+function buttonSubPressed(){
+    display.innerHTML = currentValue.concat("-");
+    currentValue = display.innerHTML;
+}
+function buttonmultiPressed(){
+    display.innerHTML = currentValue.concat("*");
+    currentValue = display.innerHTML;
+}
+function buttondivisPressed(){
+    display.innerHTML = currentValue.concat("/");
+    currentValue = display.innerHTML;
+}
+
